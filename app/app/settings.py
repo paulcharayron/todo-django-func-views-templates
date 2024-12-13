6392e8b9f7fd2d7748696c467b165b256853da1e
paulcharayron <93098084+paulcharayron@users.noreply.github.com>
2024-12-13 18:34:59 +0100

    custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Project apps
     "core",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"
---
+++ app/user/serializers.py
@@ -0,0 +1,33 @@
+"""
+Serializers for user-related functionality.
+"""
+
+from django.contrib.auth import (
+    get_user_model,
+)
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for user object."""
+
+    class Meta:
+        model = get_user_model()
+        fields = ["email", "password", "name"]
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        """Create a user with encrypted password and return the user."""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user and return it."""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
